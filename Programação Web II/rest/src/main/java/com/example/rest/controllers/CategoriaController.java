45613cd846409b7c5b1971c57b2e2f86fc1d2998
Matheus Filipe dos Santos Reinert <matheus.reinert@protonmail.com>
2022-11-04 19:56:37 -0300

    oops

--- Programação Web II/rest/src/main/java/com/example/rest/controllers/CategoriaController.java
+++ Programação Web II/rest/src/main/java/com/example/rest/controllers/CategoriaController.java
@@ -4,6 +4,7 @@ import com.example.rest.entities.Categoria;
 import com.example.rest.repositories.CategoriaRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
