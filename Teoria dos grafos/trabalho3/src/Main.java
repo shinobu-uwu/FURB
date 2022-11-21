bb7266e5471be0edeaf5349a386075931c0e9b69
Matheus Filipe dos Santos Reinert <matheus.reinert@protonmail.com>
2022-11-20 21:22:24 -0300

    abc

--- Teoria dos grafos/trabalho3/src/Main.java
+++ Teoria dos grafos/trabalho3/src/Main.java
@@ -1,12 +1,11 @@
-// Matheus Filipe dos Santos Reinert
+// Matheus Filipe dos Santos Reinert e Vinícius Vanelli Silvestre Silva
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        FileReader fileReader = new FileReader("“C:\\temp\\entrada.in");
+        FileReader fileReader = new FileReader("C:\\temp\\entrada.in");
         String entrada = "";
         int c;
 
@@ -19,7 +18,7 @@ public class Main {
         int grau = Integer.parseInt(linhas[0].split(" ")[0]);
         int nArestas = Integer.parseInt(linhas[0].split(" ")[1]);
 
-        int[][] grafo = new int[grau][grau];
+        // Só precisamos da matriz de custo para cacular a árvore geradora mínima
         int[][] matrizCusto = new int[grau][grau];
 
         // Inicializar matriz de custo com "infinito"
@@ -29,15 +28,14 @@ public class Main {
             }
         }
 
+        // Montar a matriz de custo
         for (int i = 1; i <= nArestas; i++) {
             String[] parametros = linhas[i].split(" ");
             int u = Integer.parseInt(parametros[0]);
             int v = Integer.parseInt(parametros[1]);
             int w = Integer.parseInt(parametros[2]);
 
-            // como não estamos trabalhando com um digrafo temos que (u,v) = (v,u)
-            grafo[u - 1][v - 1] += 1;
-            grafo[v - 1][u - 1] += 1;
+            // Como estamos trabalhando com um grafo não dirigido temos que (u,v) = (v,u)
             matrizCusto[u - 1][v - 1] = w;
             matrizCusto[v - 1][u - 1] = w;
         }
@@ -49,23 +47,23 @@ public class Main {
 
         for (int k = 0; k < grau - 1; k++) {
             int min = Integer.MAX_VALUE;
-            // armazenar o vértice com o menor caminho
+            // Armazenar o vértice com o menor caminho
             int x = -1;
             int y = -1;
 
             for (int i = 0; i < grau; i++) {
                 for (int j = 0; j < grau; j++) {
-                    if (matrizCusto[i][j] < min) {
-                        if (ehArestaValida(i, j, visitados)) {
-                            min = matrizCusto[i][j];
-                            x = i;
-                            y = j;
-                        }
+                    if (matrizCusto[i][j] < min && ehArestaValida(i, j, visitados)) {
+                        min = matrizCusto[i][j];
+                        x = i;
+                        y = j;
                     }
                 }
             }
 
-            if (x != -1 && y != -1) {
+            if (x != -1 && y != -1) {  // encontramos um caminho mínimo válido na iteração
+                // como só precisamos calcular o custo mínimo da árvore apenas somamos
+                // o custo do caminho mínimo encontrado no total
                 custoMinimo += min;
                 visitados[y] = true;
                 visitados[x] = true;
@@ -75,9 +73,9 @@ public class Main {
         System.out.println(custoMinimo);
     }
 
-    private static boolean ehArestaValida(int u, int v, boolean[] inMST) {
+    private static boolean ehArestaValida(int u, int v, boolean[] visitados) {
         if (u == v) return false;
-        if (!inMST[u] && !inMST[v]) return false;
-        else return !inMST[u] || !inMST[v];
+        if (!visitados[u] && !visitados[v]) return false;
+        else return !visitados[u] || !visitados[v];
     }
 }
